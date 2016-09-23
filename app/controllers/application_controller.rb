d7f7639b447d9d58d3f94cce8fe3a28ed419f5c0
Daniel Riley <driley3@gmail.com>
2016-09-23 16:29:20 -0400

    Added API contstraints and refactored routes to use HTTP Accept header for versioning

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,11 @@ class ApplicationController < ActionController::API
 
   respond_to :json
 
+  before_action :ensure_json_request
+
+  def ensure_json_request
+    return if request.format == :json
+    render :nothing => true, :status => 406
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-   namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
-    scope module: :v1 do
+
+  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+    scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :comments
     end
+
   end
+
 end
