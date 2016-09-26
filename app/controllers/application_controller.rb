4e1846a409fda4898cddc07ceea01837acdc0818
Daniel Riley <driley3@gmail.com>
2016-09-26 15:24:09 -0400

    Updated users_controller tests for create/update/destroy actions, and users_controller actions were added to pass all tests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,10 @@
 class ApplicationController < ActionController::API
+  include ActionController::RequestForgeryProtection
+
+
 
   respond_to :json
 
-  before_action :ensure_json_request
-
-  def ensure_json_request
-    return if request.format == :json
-    render :nothing => true, :status => 406
-  end
   
+
 end
