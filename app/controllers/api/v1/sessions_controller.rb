a0e083ac9f8114b7b1e760510f06d168708a2d58
Daniel Riley <driley3@gmail.com>
2016-10-12 17:18:04 -0500

    added sessions and current_user controller to handle auth token

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,21 @@
+class Api::V1::SessionsController < ApplicationController
+  skip_before_action :authenticate, only: [:create]
+
+  def create
+    user = User.find_by(email: auth_params[:email])
+    if user && user.authenticate(auth_params[:password])
+      jwt = Auth.issue({user: user.id})
+      render json: {jwt: jwt}
+    else
+      render json: {:errors=>
+        [{:detail=>"incorrect email or password",
+          :source=>{:pointer=>"user/err_type"}}
+        ]}, status: 404
+    end
+  end
+
+  private
+    def auth_params
+      params.require(:auth).permit(:email, :password)
+    end
+end
