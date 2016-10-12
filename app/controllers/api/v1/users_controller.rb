b402963be8843ddaa7583744bd66b112a4d67ada
Daniel Riley <driley3@gmail.com>
2016-10-12 16:56:10 -0500

    updated user model and user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,39 +1,31 @@
 class Api::V1::UsersController < ApplicationController
   respond_to :json
-  before_action :authenticate_user
+  include ErrorSerializer
+  skip_before_action :authenticate, only: [:create]
+
+
+  def index
+    render json: User.all
+  end
 
   def show
-    respond_with User.find(params[:id])
+    render json: User.find(params[:id])
   end
 
   def create
-    @user = User.from_token_request(user_params)
-    if @user.save
-      render json: @user, status: 201, location: [:api, @user]
+    user = User.new(user_params)
+    if user.save
+      render json: {}, status: 200
     else
-      render json: { errors: @user.errors }, status: 422
+      render json: ErrorSerializer.serialize(user.errors), status: 422
     end
-  end
-
-  def update
-    user = User.find(params[:id])
-
-    if user.update(user_params)
-      render json: user, status: 200, location: [:api, user]
-    else
-      render json: { errors: user.errors }, status: 422
-    end
-  end
-
-  def destroy
-    user = User.find(params[:id])
-    user.destroy
-    head 204
   end
 
   private
 
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation)
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
     end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,9 @@
 class User < ApplicationRecord
   has_many :jobs
 
-  validates :auth_id, presence: true
-  validates :email, presence: true
+  has_secure_password
 
-  def self.from_token_request(request)
-    email = request.params["auth"] && request.params["auth"]["email"]
-    auth_id = request.params["auth"] && request.params["auth"]["identities"]["user_id"]
-    self.create_with(email).find_or_create_by(auth_id)
-  end
+  validates :email, format: { with: /\A[^@\s]+@([^@.\s]+\.)+[^@.\s]+\z/ }
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
 end
